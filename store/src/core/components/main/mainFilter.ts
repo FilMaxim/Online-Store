04caef9eaf46d32dbfc7b01ef5621b1687f236cd
Oksana Kalinina <ksan4ik92@gmail.com>
2022-12-22 19:11:19 +0300

    feat:добавлены методы и св-ва в констр для фильтр

--- store/src/core/components/main/mainFilter.ts
+++ store/src/core/components/main/mainFilter.ts
@@ -1,19 +1,91 @@
 import Component from '../../templates/components';
 import Filter from './filter';
 import mainCards from './mainCards';
+import { obj } from '../../../pages/main';
+
+export interface parametrObj {
+    category: string[];
+    brand: string[];
+}
 
 export default class mainFilter extends Component {
+    static parametrs: parametrObj = { category: [], brand: [] }; //где будем хранить чеки по фильтрам
+
+    //ищем продукты в объекте по фильтрам
+    static searchCardsFilter(prod: parametrObj) {
+        let objProducts = obj.products;
+
+        if (prod.category.length !== 0)
+            objProducts = objProducts.filter((item) => {
+                if (prod.category.indexOf(item.category) === -1) return false;
+                return true;
+            });
+        if (prod.brand.length !== 0)
+            objProducts = objProducts.filter((item) => {
+                if (prod.brand.indexOf(item.brand) === -1) return false;
+                return true;
+            });
+
+        return objProducts;
+    }
+
+    //создаем и устанаваливаем url
+    static createHashFilter(obj: parametrObj) {
+        let res = '';
+        res = obj.category.length !== 0 ? `category=${obj.category.join('|')}` : '';
+        res =
+            res +
+            (obj.brand.length !== 0
+                ? res === ''
+                    ? `brand=${obj.brand.join('|')}`
+                    : `?&brand=${obj.brand.join('|')}`
+                : '');
+        res = res === '' ? '' : `?${res}`;
+        window.location.hash = res;
+    }
+
     mainArea: mainCards; //для связи фильтра с карточками (чтобы при клике по фильтрам менялось поле с карточками)
-    constructor(tagName: string, className: string, mainAreaCards: mainCards) {
+    checkFilter: parametrObj;
+    constructor(tagName: string, className: string, mainAreaCards: mainCards, checkFilter: parametrObj) {
         super(tagName, className);
         this.mainArea = mainAreaCards;
+        this.checkFilter = checkFilter;
+        console.log(checkFilter);
     }
     createFilterArea() {
-        this.container.append(new Filter('div', 'filterBlock', 'category').render());
-        this.container.append(new Filter('div', 'filterBlock', 'brand').render());
+        console.log(mainFilter.parametrs);
+
+        this.container.append(
+            new Filter('div', 'filterBlock category', 'category', this.checkFilter.category).render()
+        );
+        this.container.append(new Filter('div', 'filterBlock brand', 'brand', this.checkFilter.brand).render());
     }
+
+    //по клику на фильтры формируем массив с параметрами для запроса
+    clickCheckFilter(e: Event) {
+        const target = e.target as Element;
+        if (!target.matches('input')) return;
+        const targetCheck = e.target as HTMLInputElement;
+
+        if (targetCheck.checked) {
+            if (targetCheck.closest('.category')) mainFilter.parametrs['category'].push(targetCheck.id);
+            else mainFilter.parametrs['brand'].push(targetCheck.id);
+        } else {
+            if (targetCheck.closest('.category'))
+                mainFilter.parametrs['category'].splice(mainFilter.parametrs['category'].indexOf(targetCheck.id), 1);
+            else mainFilter.parametrs['brand'].splice(mainFilter.parametrs['category'].indexOf(targetCheck.id), 1);
+        }
+
+        //перерисовываем карточки
+        this.mainArea.cards = mainFilter.searchCardsFilter(mainFilter.parametrs);
+        this.mainArea.createMainCards();
+
+        mainFilter.createHashFilter(mainFilter.parametrs); //меняем url
+    }
+
     render() {
         this.createFilterArea();
+        this.container.addEventListener('click', this.clickCheckFilter.bind(this)); //привязываем контекст, чтобы при клике по фильтрам менялось поле с карточками, которое задаем в конструкторе
         return this.container;
     }
 }
--- store/src/pages/main/index.ts
+++ store/src/pages/main/index.ts
@@ -1,10 +1,13 @@
 import './main.css';
 import Page from '../../core/templates/page';
-import * as obj from './products.json';
 
 import mainCards from '../../core/components/main/mainCards';
 import { Product } from '../../core/components/main/card';
 import mainFilter from '../../core/components/main/mainFilter';
+import { parametrObj } from '../../core/components/main/mainFilter';
+import * as objProducts from './products.json';
+
+export const obj = objProducts;
 
 class MainPage extends Page {
     static TextObject = {
@@ -12,11 +15,31 @@ class MainPage extends Page {
     };
     products: Product[];
     mainAreaCards: mainCards;
+    checkFilter: parametrObj;
 
-    constructor(id: string, products: Product[]) {
+    //делаем из строки хеша объект для фильтра
+    static createObjectParametrs(str: string): parametrObj {
+        if (str[0] !== '?') return { category: [], brand: [] };
+        str = str.slice(1);
+
+        const reg = new RegExp('%20', 'gi');
+        str = str.replace(reg, '');
+
+        const res: string[][] = str.split('?&').map((item) => item.split('='));
+        const newRes: parametrObj = { category: [], brand: [] };
+        res.forEach((item) => {
+            if (item[0] === 'category') newRes[item[0]] = item[1].split('|');
+            if (item[0] === 'brand') newRes[item[0]] = item[1].split('|');
+        });
+
+        return newRes;
+    }
+
+    constructor(id: string) {
         super(id);
-        this.products = products;
-        this.mainAreaCards = new mainCards('ul', 'products-items', products);
+        this.checkFilter = MainPage.createObjectParametrs(id);
+        this.products = mainFilter.searchCardsFilter(this.checkFilter); //вызываем метод из фильтра, который ищет объекты из массива с товарами
+        this.mainAreaCards = new mainCards('ul', 'products-items', this.products);
     }
 
     createMainPage() {
@@ -26,7 +49,7 @@ class MainPage extends Page {
         appStorePage.classList.add('app-store-page');
         main.append(appStorePage);
 
-        const filter = new mainFilter('div', 'filter', this.mainAreaCards);
+        const filter = new mainFilter('div', 'filter', this.mainAreaCards, this.checkFilter);
 
         const products = document.createElement('div');
         products.classList.add('products');
@@ -45,8 +68,8 @@ class MainPage extends Page {
     render() {
         const title = this.createHeaderTitle(MainPage.TextObject.MainTitle);
         this.container.append(title);
+
         this.container.append(this.createMainPage());
-        console.log(obj.products);
 
         return this.container;
     }
